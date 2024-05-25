f90772647e872fd3d352146f4d44d984e519d09a
Roni Hokkanen <roni.hokkanen@protonmail.com>
2024-05-25 15:55:22 +0300

    Add hyprctl clients command

--- src/hyprland_ipc/data.nim
+++ src/hyprland_ipc/data.nim
@@ -1,4 +1,4 @@
-import jsony, shared
+import jsony, shared, strformat
 
 type
   DataCommands* = enum
@@ -14,6 +14,24 @@ type
     Binds
     Animations
 
+  WorkspaceBasic* = ref object
+    id*: uint32
+
+  Client* = object
+    address*: Address # Hex string of 48-bit unsigned int
+    mapped*, hidden*: bool
+    at*, size*: tuple[x, y: int]
+    floating*: bool
+    monitor*: int
+    class*, title*, initialClass*, initialTitle*: string
+    pid*: int
+    xwayland*, pinned*, fullscreen*: bool
+    fullscreenMode*: int # TODO: Find out what this int means and make it an enum
+    fakeFullscreen*: bool
+    grouped*: seq[string] # List of address hex strings that this window is grouped with
+    swallowing*: string # TODO: Probably an address of the swallowed window or 0x0 when not swallowing
+    focusHistoryID*: int
+
 proc `$`*(cmd: DataCommands): string =
   result = case cmd
   of Monitors: "monitors"
@@ -28,14 +46,19 @@ proc `$`*(cmd: DataCommands): string =
   of Binds: "binds"
   of Animations: "animations"
 
-proc callHyprctlDataCmd*(cmd: DataCommands) =
-  let msg = writeToSocket(
+proc getDataCmdJson(cmd: DataCommands): string =
+  let dataCmdString = fmt"j/{cmd}"
+
+  let msg = sendJsonRequest(
     getSocketPath(kCommand),
-    command(
-      kJson,
-      cmd.toJson()
-    )
+    command(kJson, dataCmdString)
   )
 
-type WorkspaceBasic* = ref object of RootObj
-  id*: uint32
+  if not msg.success:
+    raise newException(HyprlandDefect, fmt"{cmd} command returned non-ok status: {msg.response}")
+  else:
+    return msg.response
+
+proc hyprctlClients*(): seq[Client] =
+  let reply = getDataCmdJson(Clients)
+  return fromJson(reply, seq[Client])
