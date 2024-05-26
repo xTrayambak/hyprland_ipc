72443e574d311108ed82c4937ac6b730c6d6a65c
Roni Hokkanen <roni.hokkanen@protonmail.com>
2024-05-26 20:42:03 +0300

    Declare string conversion of commands in the enum instead of using conversion procs. Add almost all Json data commands (commands enum has comments about ones that aren't implemented).

--- src/hyprland_ipc/data.nim
+++ src/hyprland_ipc/data.nim
@@ -1,28 +1,36 @@
-import jsony, shared, strformat
+import jsony, shared, strformat, tables
 
 type
-  DataCommands* = enum
-    Monitors
-    AllMonitors
-    Workspaces
-    ActiveWorkspace
-    Clients
-    ActiveWindow
-    Layers # TBD
-    Devices  # TBD
-    Version
-    CursorPosition
-    Binds # TBD
-    Animations # TBD
-    ConfigErrors # Probably not that useful
-    Decorations # Probably not that useful
-    GetOption # TBD
-    GlobalShortcuts # TBD
-    Instances # Probably not that useful
-    Layouts # Probably not that useful
-    Splash # Probably not that useful
-    SystemInfo # Probably not that useful
-    WorkspaceRules # TBD
+  DataCmdKind* = enum
+    Monitors = "monitors"
+    Workspaces = "workspaces"
+    ActiveWorkspace = "activeworkspace"
+    Clients = "clients"
+    ActiveWindow = "activewindow"
+    Layers = "layers"
+    Devices = "devices"
+    Version = "version"
+    CursorPosition = "cursorposition"
+    Binds = "binds"
+    Animations = "animations"
+    ConfigErrors = "configerrors" # Probably not that useful
+    Decorations = "decorations" # Probably not that useful
+    GetOption = "getoption" # Getting the actual option value is not implemented yet
+    GlobalShortcuts = "globalshortcuts" # Could be useful when apps actually start using the shortcuts xdg portal
+    Instances = "instances" # Probably not that useful
+    Layouts = "layouts"
+    Splash = "splash" # Probably not that useful
+    SystemInfo = "systeminfo" # Probably not that useful
+    WorkspaceRules = "workspacerules"
+
+  DataCommand* = object
+    case kind*: DataCmdKind
+    of GetOption:
+      name: string
+    of Monitors:
+      all: bool # Includes disabled monitors
+    else:
+      discard
 
   BasicWorkspaceInfo* = object # This is only for the deserialization of the monitor object
     id*: int
@@ -31,7 +39,7 @@ type
   Vector2D* = tuple
     x, y: int
 
-  Rectangle = tuple
+  Rectangle* = tuple
     x, y, w, h: int
 
   HyprlandVersion* = object
@@ -39,8 +47,10 @@ type
     dirty: bool
     commit_message*, commit_date*, tag*: string
     commits*: int
-    # TODO: Missing an array called flags, whatever that is. Probably not important
+    # TODO: Missing an array called flags, whatever that is. Probably not important.
 
+  # TODO: Many objects in this section could benefit from using Option types, because many hyprctl commands return json
+  # that doesn't have all the fields in it (e.g. workspacerules). Some types could also use existing types from dispatch.nim.
   Client* = object
     address*: Address # Hex string of 48-bit unsigned int
     mapped*, hidden*: bool
@@ -78,23 +88,78 @@ type
     currentFormat*: string
     availableModes*: seq[string]
 
-proc `$`*(cmd: DataCommands): string =
-  result = case cmd
-  of Monitors: "monitors"
-  of AllMonitors: "monitors all"
-  of Workspaces: "workspaces"
-  of ActiveWorkspace: "activeworkspace"
-  of Clients: "clients"
-  of ActiveWindow: "activewindow"
-  of Layers: "layers"
-  of Devices: "devices"
-  of Version: "version"
-  of CursorPosition: "cursorposition"
-  of Binds: "binds"
-  of Animations: "animations"
-  else: "NOT IMPLEMENTED" # TODO: Add strings even for commands without parsing implemented
+  LayerEnum* = enum # Enum for future use with LayersOnMonitor
+    Background # Wallpaper
+    Bottom # Normal app windows are between Bottom and Tap (but idk what uses Bottom)
+    Top # Bars and desktop ui (drawn on top of normal apps)
+    Overlay # Probably drawn on top of the entire monitor framebuffer
 
-proc getDataCmdJson(cmd: DataCommands): string =
+  WindowOnLayer* = object # This object represents a window and its dimensions
+    address*: Address
+    x*, y*, w*, h*: int
+    namespace*: string
+  # TODO: Currently this matches the json structure given by the layers command, but the structure is a bit hard
+  # to work with. The structure could be changed to layersOnMonitor[monitor][layerEnum], where layerEnum is
+  LayersOnMonitor* = Table[string, # Monitor name
+                     Table[string, # Levels key
+                     Table[string, # Level number (int as string)
+                           seq[WindowOnLayer]]]] # Actual layer (seq because multiple windows can occupy the same layer)
+
+  Mouse* = object
+    address*: Address
+    name*: string
+    defaultSpeed*: float
+
+  Keyboard* = object
+    address*: Address
+    name*, rules*, model*, layout*, variant*, options*, active_keymap*: string
+    main*: bool
+
+  DeviceList* = object
+    mice*: seq[Mouse]
+    keyboards*: seq[Keyboard]
+    # TODO: Missing tablets and touch (touchscreens) fields
+  
+  Option* = object
+    option*: string
+    # TODO: Add <typeName>: value field. Requires custom parsing, because there are many types
+    set*: bool
+
+  Bind* = object
+    locked*, mouse*, release*, repeat*, nonConsuming*: bool
+    modmask*: int # Proper representation for this would be a set[modEnum] where modEnum contains the mod keys
+    submap*, key*: string
+    keycode*: int # Idk what this is and why its 0 for all my binds
+    catchAll*: bool
+    dispatcher*, arg*: string
+
+  Animation* = object
+    name*: string
+    overridden*: bool
+    bezier*: string
+    enabled*: bool
+    speed*: float
+    style*: string
+
+  WorkspaceRule* = object
+    # TODO: Parse workspaceStrings into WorkspaceIdentifier using for example nitely/nim-regex
+    workspaceString*, monitor*: string 
+    default*: bool
+    gapsIn*, gapsOut*, borderSize*: int
+    border*, shadow*, rounding*, decorate*, persistent*: bool
+    onCreatedEmpty*, defaultName*: string
+
+proc `$`*(cmd: DataCommand): string =
+  case cmd.kind
+  # Commands with arguments
+  of GetOption: result = fmt"getoption {cmd.name}"
+  of Monitors:
+    result = $cmd.kind
+    if cmd.all: result &= " all"
+  # Commands without arguments
+  else: result = $cmd.kind
+
+proc getDataCmdJson(cmd: DataCommand): string =
   let dataCmdString = fmt"j/{cmd}"
 
   let msg = sendJsonRequest(
@@ -107,15 +172,21 @@ proc getDataCmdJson(cmd: DataCommands): string =
   else:
     return msg.response
 
-proc getDataAsObj[T](dataCmd: DataCommands): T =
+proc getDataAsObj[T](dataCmd: DataCommand): T =
   let reply = getDataCmdJson(dataCmd)
   return fromJson[T](reply, typedesc[T])
 
-proc getClients*(): seq[Client] = getDataAsObj[seq[Client]](Clients)
-proc getActiveWindow*(): Client = getDataAsObj[Client](ActiveWindow)
-proc getWorkspaces*(): seq[Workspace] = getDataAsObj[seq[Workspace]](Workspaces)
-proc getActiveWorkspace*(): Workspace = getDataAsObj[Workspace](ActiveWorkspace)
-proc getCursorPosition*(): Vector2D = getDataAsObj[Vector2D](CursorPosition)
-proc getVersion*(): HyprlandVersion = getDataAsObj[HyprlandVersion](Version)
-proc getMonitors*(): seq[Monitor] = getDataAsObj[seq[Monitor]](Monitors)
-proc getAllMonitors*(): seq[Monitor] = getDataAsObj[seq[Monitor]](AllMonitors)
+proc getClients*(): seq[Client] = getDataAsObj[seq[Client]] DataCommand(kind: Clients)
+proc getActiveWindow*(): Client = getDataAsObj[Client] DataCommand(kind: ActiveWindow)
+proc getWorkspaces*(): seq[Workspace] = getDataAsObj[seq[Workspace]] DataCommand(kind: Workspaces)
+proc getActiveWorkspace*(): Workspace = getDataAsObj[Workspace] DataCommand(kind: ActiveWorkspace)
+proc getCursorPosition*(): Vector2D = getDataAsObj[Vector2D] DataCommand(kind: CursorPosition)
+proc getVersion*(): HyprlandVersion = getDataAsObj[HyprlandVersion] DataCommand(kind: Version)
+proc getMonitors*(all: bool): seq[Monitor] = getDataAsObj[seq[Monitor]] DataCommand(kind: Monitors)
+proc getLayers*(): LayersOnMonitor = getDataAsObj[LayersOnMonitor] DataCommand(kind: Layers)
+proc getDevices*(): DeviceList = getDataAsObj[DeviceList] DataCommand(kind: Devices)
+proc getOption*(option: string): Option = getDataAsObj[Option] DataCommand(kind: GetOption, name: option)
+proc getBinds*(): seq[Bind] = getDataAsObj[seq[Bind]] DataCommand(kind: Binds)
+proc getAnimations*(): seq[Animation] = getDataAsObj[seq[Animation]] DataCommand(kind: Animations)
+proc getWorkspaceRules*(): seq[WorkspaceRule] = getDataAsObj[seq[WorkspaceRule]] DataCommand(kind: WorkspaceRules)
+proc getLayouts*(): seq[string] = getDataAsObj[seq[string]] DataCommand(kind: Layouts)
